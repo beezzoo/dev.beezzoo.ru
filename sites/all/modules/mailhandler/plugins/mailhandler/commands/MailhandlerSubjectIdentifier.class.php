332010b20dfcc4f6209a18e574ac8c8e81baf8b6
beezzoo <beezzoo@gmail.com>
2013-11-07 14:17:43 +0400

    Ferst commin

---
+++ sites/all/modules/mailhandler/plugins/mailhandler/commands/MailhandlerSubjectIdentifier.class.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @file
+ * MailhandlerSubjectIdentifier class.
+ */
+
+class MailhandlerSubjectIdentifier extends MailhandlerCommands {
+
+  /**
+   * Parse the subject for a Case ID.
+   */
+  function process(&$message, $source) {
+
+    $splitmail = array();
+    $splitmail = explode("-----(Please reply above this line)-----",$message['origbody']);
+
+    //strip everything after this text
+    $message['origbody'] = $splitmail[0];
+
+    //grab case_id from subject line
+    $message['case_id'] = substr($message['subject'], strpos($message['subject'], 'case #') + 6);
+  }
+
+  function getMappingSources() {
+    $sources = array();
+
+      $sources['case_id'] = array(
+        'title' => t('Case ID'),
+        'description' => t('The original Case ID'),
+      );
+    return $sources;
+  }
+}
+?>
